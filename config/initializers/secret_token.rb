f6cf66c8c66941783cfa8bb6dfd6cce52bfa6201
hugo <hugokinghall@gmail.com>
2011-11-14 18:30:49 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetMeMyBus::Application.config.secret_token = 'ff3586d149c325faec3dafe6be12e7b577f9cb63c6bcc4e1c1455fee744b3a2b2bf619614c1fea721ea6ac6401bfa310567fb6586344d2e7c4d45544f692b050'
