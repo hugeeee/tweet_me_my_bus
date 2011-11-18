8ba3cfe3dba75c704df6c5c75ba576c7bb96eeb1
hugo <hugokinghall@gmail.com>
2011-11-18 18:24:25 +0000

    all tests up to destroying session are working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 TweetMeMyBus::Application.routes.draw do
   
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/', :to => 'pages#home'
   match '/about',   :to => 'pages#about'
